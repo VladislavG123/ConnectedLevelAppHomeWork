6d72df628ffa445ab78bb244ee0a591edddb08ec
Владислав Горохов <gbg.0101@mail.ru>
2019-04-17 17:13:47 +0600

    Initial

---
+++ ConnectedLevelHomeWork2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ConnectedLevelHomeWork2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.HeadLabel = new System.Windows.Forms.Label();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(34, 96);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(226, 20);
+            this.loginTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(34, 172);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(226, 20);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // HeadLabel
+            // 
+            this.HeadLabel.AutoSize = true;
+            this.HeadLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 17.25F, System.Drawing.FontStyle.Bold);
+            this.HeadLabel.Location = new System.Drawing.Point(12, 18);
+            this.HeadLabel.Name = "HeadLabel";
+            this.HeadLabel.Size = new System.Drawing.Size(257, 28);
+            this.HeadLabel.TabIndex = 2;
+            this.HeadLabel.Text = "ConnectedLevelApp";
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.75F);
+            this.loginLabel.Location = new System.Drawing.Point(30, 73);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(53, 20);
+            this.loginLabel.TabIndex = 3;
+            this.loginLabel.Text = "Login";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.75F);
+            this.passwordLabel.Location = new System.Drawing.Point(30, 149);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(86, 20);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.LoginButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.LoginButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.25F);
+            this.LoginButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LoginButton.Location = new System.Drawing.Point(80, 214);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(113, 39);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Next";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 304);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.HeadLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Connected Level Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label HeadLabel;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button LoginButton;
+    }
+}
+
---
+++ ConnectedLevelHomeWork2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectedLevelHomeWork2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if (loginTextBox.Text != "" && passwordTextBox.Text != "")
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["appConnection"].ConnectionString; ;
+                var stringBuilder = new SqlConnectionStringBuilder
+                {
+                    ConnectionString = connectionString,
+                    UserID = loginTextBox.Text,
+                    Password = passwordTextBox.Text
+                };
+
+                using (var connection = new SqlConnection(stringBuilder.ConnectionString))
+                {
+                    MessageBox.Show("Сonnection established!");
+                }
+            }
+        }
+    }
+}
